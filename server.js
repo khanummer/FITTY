d36717d0c52416d8ba5da4d4f20b56f3f3d68f6f
Ummer Khan <ummernkhangit@gmail.com>
2019-01-28 15:40:19 -0800

    some css, login, register and logout working

--- server.js
+++ server.js
@@ -3,19 +3,32 @@ const app = express();
 require('./db/db');
 const bodyParser = require('body-parser');
 const outfitController = require('./controllers/outfits');
+const authenticationController =require('./controllers/authentication');
 const methodOverride = require('method-override');
+const session = require('express-session');
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(session({
+    secret: 'THIS IS A RANDOM SECRET STRING',
+    resave: false,
+    saveUninitialized: false
+}))
 
 
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/outfits', outfitController);
-
+app.use('/authentication', authenticationController);
 
 
 
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index.ejs', {
+        message: req.session.message,
+        loggedUser: req.session.username
+    })
 });
 
 
